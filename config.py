295e29afba666a7ec49e856163c089159461d4f2
ngshircheng <ngsc-wm22@student.tarc.edu.my>
2025-12-09 09:02:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.ct24yaimu40d.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
-customregion = "us-east-1"
-
+custombucket = "ngshircheng-bucket"
+customregion = "us-east-1a"
